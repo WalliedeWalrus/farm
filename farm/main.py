9f1b7f317d183b97d3df8d55fae5b26f734fef22
WalliedeWalrus <137280431+WalliedeWalrus@users.noreply.github.com>
2024-01-27 16:26:26 +0100

    Update main.py

--- farm/main.py
+++ farm/main.py
@@ -4,18 +4,30 @@ from flask import Flask, render_template, redirect, url_for
 # Create a Flask app inside `app`
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
-    return render_template("index.html", title="Index")
+    return render_template("index.html", name_browser_tab="Index")
 
-@app.route("/home", methods=["GET"])
+
+@app.route("/home")
 def home():
-    return redirect(url_for("index"), 302)
+    return redirect(url_for("index"))
 
-@app.route("/animals", methods=["GET"])
-def about():
-    return render_template("animals.html", title="Animals")
 
-@app.route("/farm", methods=["GET"])
+@app.route("/about")
 def about():
-    return render_template("farm.html", title="Farm")
+    return render_template("about.html", name_browser_tab="About")
+
+
+@app.route("/cow")
+def cow():
+    return render_template("cow.html", name_browser_tab="Cow")
+
+
+@app.route("/dog")
+def dog():
+    return render_template("dog.html", name_browser_tab="Dog")
+
+@app.route("/pig")
+def pig():
+    return render_template("pig.html", name_browser_tab="Pig")
