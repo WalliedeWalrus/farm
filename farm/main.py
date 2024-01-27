15b533069bff4a36016c3349545e5e10aa3cbb8a
WalliedeWalrus <137280431+WalliedeWalrus@users.noreply.github.com>
2024-01-27 13:38:50 +0100

    Update main.py

--- farm/main.py
+++ farm/main.py
@@ -4,6 +4,15 @@ from flask import Flask, render_template, redirect, url_for
 # Create a Flask app inside `app`
 app = Flask(__name__)
 
+@app.route("/home", methods=["GET"])
+def home():
+    return redirect(url_for("index"), 302)
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", title="Index")
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html", title="About")
