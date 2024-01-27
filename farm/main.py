28dbf7c02a2f669ae226c829906e5fdef85b414b
WalliedeWalrus <137280431+WalliedeWalrus@users.noreply.github.com>
2024-01-27 13:16:07 +0100

    Update main.py

--- farm/main.py
+++ farm/main.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, world! its me again are you still there still am new version'
+    return 'Hello, world! its me again are you still there still am new version still changing'
